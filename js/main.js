0cb867010e4728853f6db8590e67bd682546d6fe
Mikhail <schaslivchikm@gmail.com>
2022-01-09 16:44:23 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-const number1 = Number(prompt("Enter First Number: "));
-const number2 = Number(prompt("Enter Second Number: "));
+// const number1 = Number(prompt("Enter First Number: "));
+// const number2 = Number(prompt("Enter Second Number: "));
 
-if (number1 > number2) {
-  console.log(number2);
-} else if (number1 < number2) {
-  console.log(number1);
+// if (number1 > number2) {
+//   console.log(number2);
+// } else if (number1 < number2) {
+//   console.log(number1);
+// } else {
+//   console.log(number1 + " = " + number2);
+// }
+
+const userAge = Number(prompt("Enter Age User: "));
+
+if (userAge >= 18 && userAge <= 65) {
+  console.log("full price");
 } else {
-  console.log(number1 + " = " + number2);
+  console.log("half price");
 }
