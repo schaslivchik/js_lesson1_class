7fea42906c80de1808e5060f2d0cb01ab2a7bdeb
Mikhail <schaslivchikm@gmail.com>
2022-01-09 19:27:08 +0200

    for

--- js/loop.js
+++ js/loop.js
@@ -24,3 +24,17 @@
 // for (let i = pageCount; i > 0; --i) {
 //   console.log(i);
 // }
+
+// let sum = 0;
+// for (let i = 1; i <= 10; ++i) {
+//   sum += i;
+// }
+// console.log(sum);
+
+let akk = 1;
+for (let i = 1; i <= 10; i++) {
+  if (i % 5 === 0) {
+    akk *= i;
+    console.log(akk);
+  }
+}
