b23093ef74d87d508bd75e8c31a9e4981bd0cd90
Mikhail <schaslivchikm@gmail.com>
2022-01-15 19:30:26 +0200

    func exp

--- js/function.js
+++ js/function.js
@@ -40,13 +40,28 @@
 // console.log(sum1To5);
 // console.log(sum1To12);
 
-console.log(checkAge(12));
+// console.log(checkAge(12));
 
 /**
  * function for adult age
  * @param {number} age больше или меньше 18
  * @returns {boolean} true or false
  */
-function checkAge(age) {
-  return age > 17;
-}
+// function checkAge(age) {
+//   return age > 17;
+// }
+
+// console.log(n1Degn2(3));
+
+// function n1Degn2(n2, n1 = 1) {
+//   let degValue = 1;
+//   for (let i = n1; i <= n2; i++) {
+//     degValue *= i;
+//   }
+//   return degValue;
+// }
+
+// const xDegY = function (x, y) {
+//   return x ** y;
+// };
+// console.log(xDegY(2, 2));
